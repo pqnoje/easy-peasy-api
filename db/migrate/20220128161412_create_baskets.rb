4ed58cf55d43907f708789f3eaf769723fb294f6
Jefferson Lucena <pqnoje@hotmail.com>
2022-02-19 00:43:35 -0300

    first deploy corrected

--- db/migrate/20220128161412_create_baskets.rb
+++ db/migrate/20220128161412_create_baskets.rb
@@ -1,6 +1,5 @@
 class CreateBaskets < ActiveRecord::Migration[7.0]
   def change
-	drop_table :baskets
     create_table :baskets do |t|
       t.references :valuable, null: false, foreign_key: true
       t.references :buyer, null: false, foreign_key: true
--- db/migrate/20220128161840_create_buyers.rb
+++ db/migrate/20220128161840_create_buyers.rb
@@ -1,6 +1,5 @@
 class CreateBuyers < ActiveRecord::Migration[7.0]
   def change
-	drop_table :buyers
     create_table :buyers do |t|
       t.string :name
 
